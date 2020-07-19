99f5c4df4632141f26747f0a9ce990121a21bc5e
Robert Baumgartner <robert.baumgartner@htl-ottakring.ac.at>
2020-07-19 12:18:19 +0200

    included async call to mock up json server

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,25 @@
 <template>
   <div>
     <h1>Events</h1>
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 
 <script>
 export default {
+  async asyncData ({ $axios, error }) {
+    try {
+      const result = await $axios.get('http://localhost:3000/events')
+      return { events: result.data }
+    } catch {
+      error({ statusCode: 503, message: 'Unable to fetch events!' })
+    }
+  },
   head () {
     return {
       title: 'Event Listing'
